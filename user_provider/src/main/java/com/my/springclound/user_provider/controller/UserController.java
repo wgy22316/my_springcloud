0dfe6c49eee295fed06a31ac013b92fa32a6ca07
wugy <wu.gongyao@puscene.com>
2018-07-03 20:00:42 +0800

    first commit

---
+++ user_provider/src/main/java/com/my/springclound/user_provider/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.my.springclound.user_provider.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    public String addUser() {
+        return "success";
+    }
+}
