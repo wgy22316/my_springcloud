e70ca12ca3b47211d38ea3e7c08070bbbc54e61e
wugy <wu.gongyao@puscene.com>
2018-07-17 20:36:02 +0800

    init

--- user_provider/src/main/java/com/my/springclound/user_provider/controller/UserController.java
+++ user_provider/src/main/java/com/my/springclound/user_provider/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.my.springclound.user_provider.controller;
 
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Controller;
+
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     public String addUser() {
         return "success";
